a1039f27c2e140f781bab57a0652eb100512c442
maescristro <milfortchristian@gmail.com>
2022-10-24 20:44:16 +0200

    eau03.js

---
+++ eau03.js
@@ -0,0 +1,22 @@
+// Créez un programme qui affiche ses arguments reçus à l’envers.
+
+function reverse(array) {
+    let output = [];
+    while (array.length) {
+      output.push(array.pop());
+    }
+    return output;
+}
+
+let args = process.argv.slice(2);
+function func1() {
+    if(args == ''){
+        console.log("error");
+        return;
+    }
+    console.log(reverse(args));
+
+    
+}
+
+func1(args);
