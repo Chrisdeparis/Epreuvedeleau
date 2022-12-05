71032cc82895ae1129df7282d648b1e3756ad339
maescristro <Chrisdeparis@users.noreply.github.com>
2022-12-05 15:42:14 +0100

    Update eau06.js
    string dans string ok

--- eau06.js
+++ eau06.js
@@ -9,38 +9,43 @@
 
 // $> python exo.py 42
 // error
-let args = process.argv.slice(2);
 
-if (
-  args == '' ||
-  args[0] == '' ||
-  args[1] == '' ||
-  typeof parseInt(args[0]) == 'number'
-) {
-  console.log('error');
-  return;
+function isStringInString(haystack, needle){
+  return haystack.includes(needle);
 }
 
-const containSubstr = (str, substr) => {
-  let count = 0;
-  let i = 0;
-  let startIndex = 0;
 
-  while (i < str.length) {
-    if (substr[count] === str[i]) {
-      if (count === substr.length - 1) {
-        return true;
-      }
-      count++;
-    } else {
-      count = 0;
-      i = startIndex;
-      startIndex++;
-    }
-    i++;
-  }
-  return false;
-};
+const errorOnlyTwoArguments = new Error("error argument");
 
-let test = containSubstr(args[0], args[1]);
-console.log(test);
+function isOnlyTwoArguments(args){
+return(args.length == 2);
+}
+
+/*
+* Return 1 if no errors has been detected, else return error message
+*/
+function checkArgumentForError(args){
+let testResult = 1;
+if(!isOnlyTwoArguments(args)){
+  //1.a if none or more than two argument has been passed
+  testResult = errorOnlyTwoArguments;
+}
+return testResult;
+}
+
+
+let args = process.argv.slice(2);
+let checkResult = checkArgumentForError(args);
+let result = "error";
+
+
+//1. Arguments test
+if(checkResult !== 1){
+result = checkResult.message;
+} else {
+//2. Check if second argument is included in the first argument
+result = isStringInString(args[0], args[1]);
+}
+
+
+console.log(result);
