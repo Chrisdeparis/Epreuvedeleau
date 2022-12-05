43f1900668e709a9a2e1638f26d1bc4bf356a023
maescristro <Chrisdeparis@users.noreply.github.com>
2022-12-05 14:13:07 +0100

    Update eau04.js
    fibonnaci ok

--- eau04.js
+++ eau04.js
@@ -1,28 +1,53 @@
 // Créez un programme qui affiche le N-ème élément de la célèbre suite de Fibonacci. (0, 1, 1, 2) étant le début de la suite et le premier élément étant à l’index 0.
-let args = process.argv.slice(2);
 
-
-const calculateSum = (arr) => {
-    return arr.reduce((total, current) => {
-        return total + current;
-    }, 0);
-}
-
-function fibonacci(n){
-    const fib=[0,1];
-    let result;
-    for(let i =2; i<n; i++){
-        fib[i] = fib[ i -1] + fib[i -2];
-        
+function getFibonacciElementByIndex(index){
+    if(index <= 0){
+      return 0;
+    } else if (index == 1) {
+      return 1;
+    } else {
+      return getFibonacciElementByIndex(index - 1) + getFibonacciElementByIndex(index - 2);
     }
-    return calculateSum(fib);
-}
+  }
+  
 
-function func1(){
-    if(args <= 0 || args == '') {
-        console.log(-1);
-        return;
+
+  const errorNegative = new Error("l'Argument doit etre un entier positif.");
+  const errorNotAnInteger = new Error("l'argument n'est pas un entier.");
+  const errorWrongArguments = new Error("Mauvais arguments.");
+  const error = new Error("-1");
+  
+  function isArgumentNegative(argument){
+    return(argument<0);
+  }
+  
+  function isArgumentNotAnInteger(argument){
+    return(argument % 1 !== 0);
+  }
+  
+  function isThereOneAndOnlyOneArgument(args){
+    return(args.length == 1);
+  }
+  
+
+  const args = process.argv.slice(2);
+  
+
+  let element = -1;
+  if(isThereOneAndOnlyOneArgument(args)){
+    const indexElement = args[0];
+    if(!isArgumentNotAnInteger(indexElement)){
+        if(!isArgumentNegative(indexElement)){
+          element = getFibonacciElementByIndex(indexElement);
+        } else {
+          console.log(errorNegative.message);
+        }
+    } else {
+      console.log(errorNotAnInteger.message);
     }
-    console.log(fibonacci(args));
-}
-func1(args);
+  } else {
+    console.log(errorWrongArguments.message);
+  }
+  
+ 
+  console.log(element);
