defca2472bf573487a2ef2d5a2f277e88a4eca4b
maescristro <Chrisdeparis@users.noreply.github.com>
2022-12-14 09:24:27 +0100

    Update eau02.js

--- eau02.js
+++ eau02.js
@@ -1,39 +1,22 @@
 // Créez un programme qui affiche toutes les différentes combinaisons de deux nombre entre 00 et 99 dans l’ordre croissant.
 
-function combinaison() {
-  let result = [];
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      for (let k = 0; k < 10; k++) {
-        for (let l = 0; l < 10; l++) {
-          let num1 = `${i}${j}`;
-          let num2 = `${k}${l}`;
-          if (num1 !== num2) {
-            if (!result.includes(`${num1} ${num2}`)) {
-              if (!result.includes(`${num2} ${num1}`)) {
-                result.push(`${num1} ${num2}`);
-              } else {
-                //no add
-              }
-            } else {
-              //no add
-            }
-          }
-        }
+function combinations() {
+  // Create an empty array to store the combinations
+  const combos = [];
+
+  // Loop through the numbers 00 to 99 (inclusive)
+  for (let i = 0; i < 100; i++) {
+    for (let j = i + 1; j < 100; j++) {
+      // If the numbers are in ascending order and do not repeat,
+      // add them to the array of combinations
+      if (i < j) {
+        combos.push(`${i.toString().padStart(2, '0')} ${j.toString().padStart(2, '0')}`);
       }
     }
   }
-  return result;
+
+  // Return the array of combinations
+  return combos;
 }
 
-function displayResult(result) {
-  let stringResult = '';
-  for (let i = 0; i < result.length - 1; i++) {
-    stringResult += `${result[i]}, `;
-  }
-  stringResult += `${result[result.length - 1]}`;
-  console.log(stringResult);
-}
-
-let resultat = combinaison();
-displayResult(resultat);
+console.log(combinations());
