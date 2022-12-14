dd0d1e8a0bfbaa5974b9596d9c62df3d07e7f7cf
maescristro <Chrisdeparis@users.noreply.github.com>
2022-12-14 14:45:48 +0100

    Update eau04.js

--- eau04.js
+++ eau04.js
@@ -1,53 +1,20 @@
 // Créez un programme qui affiche le N-ème élément de la célèbre suite de Fibonacci. (0, 1, 1, 2) étant le début de la suite et le premier élément étant à l’index 0.
+// Check if the input is valid
+if (process.argv.length < 3 || isNaN(process.argv[2]) || process.argv[2] < 0) {
+  console.log(-1);
+  return;
+}
 
-function getFibonacciElementByIndex(index){
-    if(index <= 0){
-      return 0;
-    } else if (index == 1) {
-      return 1;
-    } else {
-      return getFibonacciElementByIndex(index - 1) + getFibonacciElementByIndex(index - 2);
-    }
-  }
-  
+// Get the Nth element from the command line argument
+let n = parseInt(process.argv[2]);
 
+// Define the Fibonacci sequence
+let fibonacci = [0, 1];
 
-  const errorNegative = new Error("l'Argument doit etre un entier positif.");
-  const errorNotAnInteger = new Error("l'argument n'est pas un entier.");
-  const errorWrongArguments = new Error("Mauvais arguments.");
-  const error = new Error("-1");
-  
-  function isArgumentNegative(argument){
-    return(argument<0);
-  }
-  
-  function isArgumentNotAnInteger(argument){
-    return(argument % 1 !== 0);
-  }
-  
-  function isThereOneAndOnlyOneArgument(args){
-    return(args.length == 1);
-  }
-  
+// Generate the Fibonacci sequence up to the Nth element
+for (let i = 2; i <= n; i++) {
+  fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
 
-  const args = process.argv.slice(2);
-  
-
-  let element = -1;
-  if(isThereOneAndOnlyOneArgument(args)){
-    const indexElement = args[0];
-    if(!isArgumentNotAnInteger(indexElement)){
-        if(!isArgumentNegative(indexElement)){
-          element = getFibonacciElementByIndex(indexElement);
-        } else {
-          console.log(errorNegative.message);
-        }
-    } else {
-      console.log(errorNotAnInteger.message);
-    }
-  } else {
-    console.log(errorWrongArguments.message);
-  }
-  
- 
-  console.log(element);
+// Display the Nth element
+console.log(fibonacci[n]);
